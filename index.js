e53859d4a400f0e7fed0a69b8c858c8a7cebde9d
John W <xjmwest@gmail.com>
2016-03-09 16:30:08 -0500

    First commit

--- index.js
+++ index.js
@@ -124,7 +124,10 @@ app.post('/newquestion', function (request, response) {
   }
   
   if(insertedQuestion) {
-    response.send({redirect: '/admin'+ insertedQuestion.id});
+    //response.send({redirect: '/admin'+ insertedQuestion.id});
+    //response.status(200).send('<html><body></body><script type="text/javascript">window.location.href="/some_new_path";</script></html>');
+    response.status(200);
+    response.redirect('/admin'+ insertedQuestion.id);
   }else {
     response.send({redirect: '/admin'});
   }
