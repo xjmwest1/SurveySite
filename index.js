a2a67231656012ae979f61ce40a731c73d657506
John W <xjmwest@gmail.com>
2016-03-10 11:12:38 -0500

    First commit

--- index.js
+++ index.js
@@ -43,9 +43,6 @@ app.get('/index', getRandomQuestion, function(request, response) {
 function getRandomQuestion(request, response, next) {
   var answeredQuestionIds = request.session.answeredQuestionIds || [];
   var unansweredQuestionIds = [];
-  
-  console.log('answeredQuestionIds-------------------------');
-  console.log(answeredQuestionIds);
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM question_table', function(err, questionRows) {
@@ -61,6 +58,12 @@ function getRandomQuestion(request, response, next) {
             unansweredQuestionIds.push(q.id);
           }
         });
+        
+        console.log('answeredQuestionIds-------------------------');
+        console.log(answeredQuestionIds);
+        console.log('unansweredQuestionIds-------------------------');
+        console.log(unansweredQuestionIds);
+        
         
         // pick random unanswered question
         var questionId = unansweredQuestionIds[Math.floor(Math.random() * unansweredQuestionIds.length)];
