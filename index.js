e1a3141e92cc1dd58c77db8526995082ed34c93a
John W <xjmwest@gmail.com>
2016-03-10 09:15:21 -0500

    First commit

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.cookieSession());
+app.use(app.router);
 app.use(express.static(__dirname));
 
 app.set('view engine', 'ejs');
